9a711853afcca3960c6ac56128e8bd2b6b40c175
philemon <cheruiyotphilimon6@gmail.com>
2024-09-28 07:44:50 +0300

    Add configuration files and dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { chats } = require("./data/data");
+const chats = require('./data/data');
 const dotenv = require("dotenv");
 
 const app = express();
 dotenv.config();
+const cors = require("cors");
+
+app.use(cors());
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,13 +15,13 @@ app.get("/", (req, res) => {
   res.send("API is running successfully");
 });
 
-app.get("/api/chat", (req, res) => {
+app.get("/api/chats", (req, res) => {
   res.send(chats);
 });
 
-app.get('/api/chat/:id', (req, res) => {
+app.get('/api/chats/:id', (req, res) => {
   // console.log(req.params.id);
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
   res.send(singleChat);
 });
 
