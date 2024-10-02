0b155a6cfcb04490be8a7c391390bec39796b096
philemon <cheruiyotphilimon6@gmail.com>
2024-10-02 13:57:50 +0300

    Refactor userControllers.js to improve error handling and remove unnecessary status codes

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -6,7 +6,6 @@ const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
 
   if (!name || !email || !password) {
-    res.status(400);
     throw new Error('Please enter all fields');
   }
 
@@ -14,7 +13,6 @@ const registerUser = asyncHandler(async (req, res) => {
   const userExists = await User.findOne({ email });
 
   if (userExists) {
-    res.status(400);
     throw new Error('User already exists');
   }
 
@@ -35,8 +33,7 @@ const registerUser = asyncHandler(async (req, res) => {
       token: generateToken(user._id),
     });
   } else {
-    res.status(400);
-    throw new Error('User not found');
+    throw new Error('Failed to create user');
   }
 });
 
@@ -44,7 +41,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email});
+  const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
     res.json({
@@ -55,7 +52,6 @@ const authUser = asyncHandler(async (req, res) => {
       token: generateToken(user._id),
     });
   } else {
-    res.status(401);
     throw new Error('Invalid email or password');
   }
 });
